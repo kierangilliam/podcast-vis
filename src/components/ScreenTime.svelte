5cc183fda737a52a86c01d68c473f921053864a8
Kieran Gill <kieran.gill1997@gmail.com>
2020-11-04 16:11:29 -0800

    basic donut chart

--- src/components/ScreenTime.svelte
+++ src/components/ScreenTime.svelte
@@ -1,37 +1,72 @@
 <script lang='ts'>
-    import { H3 } from '@ollopa/cedar'
-    import { onMount } from 'svelte'
-    import * as d3 from 'd3'
+	import { COLORS } from '@lib/constants'
+    import { Flex, H3, H5 } from '@ollopa/cedar'
+	import * as d3 from 'd3'
+	import { onMount } from 'svelte'
 
-    let E
-    const data = [30, 86, 168, 281, 303, 365];
+	const data = {
+		'Camera Center': 9, 
+		'Camera Left': 20, 
+		'Camera Right': 30,
+	}
 
-    // onMount(() => {
-    //     d3.select(E)
-	// 		.selectAll('div')
-	// 		.data(data)
-	// 		.enter()
-	// 		.append('div')
-	// 		.style('width', function(d) {
-	// 			return d + 'px';
-	// 		})
-	// 		.text(function(d) {
-	// 			return d;
-	// 		})
-    // })
+	const width = 450, height = 450, margin = 40
+	const radius = Math.min(width, height) / 2 - margin
+
+	onMount(() => {
+		const svg = d3.select('#data')
+			.append('svg')
+			.attr('width', width)
+			.attr('height', height)
+			.append('g')
+			.attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');		
+
+		const color = d3.scaleOrdinal()
+			.domain(d3.extent(Object.values(data)))
+			.range([COLORS.blue, COLORS.green, COLORS.purple, COLORS.red])
+
+		const pie = d3.pie().value(([_, value]) => value)
+
+		const arc = d3.arc()
+			// Donut size
+			.innerRadius(150)
+			.outerRadius(radius)
+
+		const arcData = pie(Object.entries(data))
+
+		const arcs = svg
+			.selectAll('arcs')
+			.data(arcData)
+			.enter()
+
+		arcs.append('path')
+			.attr('d', arc)
+			.attr('fill', d => color(d.data[0]))
+			.attr('stroke', COLORS.white)
+			.style('stroke-width', '4px')
+			.style('opacity', 0.7)
+
+		const legendText = arcs.append('text')
+			.attr('dx', `-2em`)
+			.attr('dy', (d, i) => `${2 * i - 2}em`)
+			.style('text-anchor', 'left')
+			.style('color', COLORS.black)
+			.text(({ data }) => data[0])
+
+		const legendColors = arcs.append("rect")
+			.attr("x", '-3.75em')
+			.attr('y', (_, i) => `${2 * i - 3}em`)
+			.attr("rx", "6px")
+			.attr("width", 20)
+			.attr("height", 20)
+			.attr('fill', d => color(d.data[0]))
+	})
 </script>
 
-<H3>Screen time</H3>
-
-<!-- <div class="chart" bind:this={E}></div> -->
-
-<style>
-	.chart :global(div) {
-		font: 10px sans-serif;
-		background-color: steelblue;
-		text-align: right;
-		padding: 3px;
-		margin: 1px;
-		color: white;
-	}
-</style>
+<Flex>
+	<div id='data'></div>
+	<div>
+		<H3>Screen time</H3>
+		<H5>On average, Joe is on screen 30% of the time per episode.</H5>
+	</div>
+</Flex>
