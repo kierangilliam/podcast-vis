1581d07e8a2b2ac8f68f5f3b98dd41bad34bb882
Kieran Gill <kieran.gill1997@gmail.com>
2020-11-23 18:35:41 -0800

    protocol buffers to reduce cos sim file size from 65mb to 16ish

--- src/components/Similarity.svelte
+++ src/components/Similarity.svelte
@@ -3,60 +3,91 @@
     import { onMount } from 'svelte'
     import SimilarityComparedAll from './SimilarityComparedAll.svelte'
     import SimilarityMatrix from './SimilarityMatrix.svelte'
-	import * as d3 from 'd3'
     import { episode } from '@lib/utils'
-    import * as pbf from 'pbf'
+    import { EpisodeSims, IDs } from '@lib/proto/ep-sim'
 
-    let data, allData, comparedAllId: string
+    let data, idLookup, allData, comparedAllIdNum: number, comparedAllId: string
+    let reverseIdLookup = (_: string): number => {}
     
-    $: comparedAllData = filterComparedAllData(allData, comparedAllId)
+    $: comparedAllData = filterComparedAllData(allData, comparedAllIdNum)
+    $: comparedAllIdNum = reverseIdLookup(comparedAllId)
 
     const filterComparedAllData = (_, __) => {
         // Return default blank 5 row array for DOM sizing purposes
-        if (!(allData && comparedAllId)) return [[], [], [], [], []]
+        if (!(allData && comparedAllIdNum)) return [[], [], [], [], []]
 
-        return allData.filter(({ id1, id2 }) => 
-                (id1 == comparedAllId) || (id2 == comparedAllId)
-            )
-            .sort((a, b) =>  b[1] - a[1])
-            .map(({ similarity, id1, id2 }) => 
+        const items = allData
+            .filter(({ idNum1, idNum2 }) => (idNum1 == comparedAllIdNum) || (idNum2 == comparedAllIdNum))
+            .map(fillInData)
+            .sort((a, b) => b.similarity - a.similairty)
+            .map(({ similarity, id1, id2, idNum1 }) => 
                 // [Other episode id, similarity, other episode high ranking tfidf words]
-                ([comparedAllId == id1 ? id2 : id1, similarity, '__'])
+                ([comparedAllIdNum == idNum1 ? id2 : id1, similarity, '__'])
             )
             // Filter non-main episodes
             // TODO should also filter in MMA?
             .filter(([id, _]) => episode(id).main)
             .slice(0, 5)
+        console.log("ITEMS", items)
+        return items
+    }
+
+    // Fill in the protobuf data (idNum1, idNum2, similarity) with more details
+    const fillInData = ({ idNum1, idNum2, similarity }) => ({
+        idNum1, idNum2,
+        id1: idLookup(idNum1),
+        id2: idLookup(idNum2),
+        similarity,
+    })
+
+    // Choose a random main episode, returns its idNum
+    const choice = () => {
+        const isNotMain = true
+        let choice
+        while (isNotMain) {
+            choice = allData[Math.floor(Math.random() * allData.length)]
+            choice = fillInData(choice)
+            if (episode(choice.id1).main) {
+                return choice.idNum1
+            }
+        }   
     }
 
     onMount(async () => {
-        // const buffer = (await fetch('./ep_sim'))
-        // console.log(buffer)
-        // const proto = new pbf.Pbf(buffer).readFields(readEpSim, {})
-        // function readEpSim(tag, data, pbf) {
-        //     if (tag === 1) data.rows = pbf.readMessage(readEpSimRow, {})
-        // }
-        // function readEpSimRow(tag, layer, pbf) {
-        //     if (tag === 1) layer.idNum1 = pbf.readFixed32()
-        //     else if (tag === 2) layer.idNum2 = pbf.readFixed32()
-        //     else if (tag === 3) layer.similarity = pbf.readFloat()
-        // }
-        // console.log(proto)
-
-        allData = (await d3.csv('./cosine_similarity.csv'))
-
-
-        // Choose a random main episode
-        const choice = () => {
-            const isNotMain = true
-            let choice
-            while (isNotMain) {
-                choice = allData[Math.floor(Math.random() * allData.length)]
-                if (episode(choice.id1).main) {
-                    return choice.id1
-                }
-            }   
+        async function loadSims() {
+            const response = await fetch('./ep_sim')
+            const bufferRes = await response.arrayBuffer();
+            // @ts-ignore
+            const pbf = new Pbf(new Uint8Array(bufferRes));
+            // @ts-ignore
+            const { rows } = EpisodeSims.read(pbf);
+            return rows
         }
+
+        // Returns a function that maps from an idNum to an id
+        async function loadIDLookup() {
+            const response = await fetch('./ep_sim_id_lookup')
+            const bufferRes = await response.arrayBuffer();
+            // @ts-ignore
+            const pbf = new Pbf(new Uint8Array(bufferRes));
+            // @ts-ignore
+            const { rows } = IDs.read(pbf);
+
+            function lookup(idNum: number): string {
+                return rows.find(item => item.idNum === idNum).id
+            }
+            
+            function reverseIdLookup (id: string): number {
+                if (!id) return
+                console.log("HERE")
+                console.log(id)
+                return rows.find(item => item.id === id).idNum
+            }
+
+            return [lookup, reverseIdLookup]
+        }
+
+        [allData, [idLookup, reverseIdLookup]] = await Promise.all([loadSims(), loadIDLookup()])
 
         data = [...Array(5)].map(_ => choice())
 
@@ -64,18 +95,19 @@
         data = data.map(id => {
                 return data.map(idInner => {
                     if (id == idInner) {
-                        return [id, 0.0]
+                        return [idLookup(id), 0.0]
                     }
 
-                    const hit = allData.find(({ id1, id2 }) => 
-                        (id1 == id && idInner == id2) || (id2 == id && idInner == id1)
+                    const hit = allData.find(({ idNum1, idNum2 }) => 
+                        (idNum1 == id && idInner == idNum2) || (idNum2 == id && idInner == idNum1)
                     )
 
-                    return [idInner, hit.similarity]
+                    return [idLookup(idInner), hit.similarity]
                 })
             })
 
-        comparedAllId = choice()
+        comparedAllIdNum = choice()
+        comparedAllId = idLookup(comparedAllIdNum)
     })
 </script>
 
