1b5cec0e6faedddbd3f5170ade263d519a197826
Kieran Gill <kieran.gill1997@gmail.com>
2020-11-24 17:38:19 -0800

    styling

--- src/components/Tooltip.svelte
+++ src/components/Tooltip.svelte
@@ -8,7 +8,7 @@
     $: tooltipStyle = tooltip && tooltipElem && (() => {
         const { x, y } = tooltip
         const { height, width } = tooltipElem.getBoundingClientRect()
-        const tooltipOffset = 15
+        const tooltipOffset = 25
         return `left: ${x - width / 2}px; top: ${y - height - tooltipOffset}px`
     })()
 </script>
@@ -18,7 +18,7 @@
         bind:this={tooltipElem} 
         class='tooltip' 
         style={tooltipStyle}
-        in:fly={{ y: 50 }}
+        transition:fly={{ y: 20, duration: 150 }}
     >
         <slot />
     </div>
@@ -26,23 +26,41 @@
 
 <style>
     .tooltip {
+        --tooltipSize: 8px;
+        --tooltipLeft: calc(50% - var(--tooltipSize) / 2);
+        --tooltipBorderWidth: 2px;
+
         position: fixed;
         z-index: 100;
         background: var(--lightGray);
-        border: 2px solid var(--black);
-        padding: var(--s-3) var(--s-4);
+        border: var(--tooltipBorderWidth) solid var(--black);
+        padding: var(--s-3) var(--s-4);        
     }
     /* Triangle */
     .tooltip:before{
         content:'';
         display: block;
+        position: absolute;
         width:0;
         height:0;
+
+        border-top: var(--tooltipSize) solid var(--black);
+        border-left: var(--tooltipSize) solid transparent; 
+        border-right: var(--tooltipSize) solid transparent;
+        
+        left: var(--tooltipLeft);        
+        bottom: calc(-1 * var(--tooltipSize));
+    }
+    .tooltip:after {
+        content: '';
+        display: block;  
         position: absolute;
-        border-top: 8px solid var(--black);
-        border-left: 8px solid transparent; 
-        border-right: 8px solid transparent;
-        left: calc(50% - 4px);        
-        bottom: -8px;
+        width: 0;
+        height: 0;
+
+        top: 100%; /* bottom: 100% if tooltip is on top */
+        left: calc(var(--tooltipLeft) + var(--tooltipBorderWidth) * 1.5);
+        border: calc(var(--tooltipSize) - var(--tooltipBorderWidth) * 1.5) solid transparent;
+        border-top-color: var(--lightGray);
     }
 </style>
