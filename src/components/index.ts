90dac510285babb7506387268ba5cf9b3be9c4be
Kieran Gill <kieran.gill1997@gmail.com>
2020-11-28 12:01:01 -0800

    add intro

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Similarity } from './episode-similarity/Similarity.svelte';
+export { default as Intro } from './intro/Intro.svelte';
 export { default as ScreenTime } from './screen-time/ScreenTime.svelte';
 export { default as Section } from './Section.svelte';
 export { default as Theme } from './Theme.svelte';
