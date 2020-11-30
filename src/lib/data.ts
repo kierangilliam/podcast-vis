1e5faf012f81e8e80f13e4bbe6c8db0237c1a638
Kieran Gill <kieran.gill1997@gmail.com>
2020-11-29 16:37:42 -0800

    fetch data off main thread

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,25 +1,33 @@
-import * as d3 from 'd3';
-import { writable } from 'svelte/store';
-import type { TopTFIDF, WordOccurrences } from './types';
+import * as Comlink from 'comlink'
+import { writable } from 'svelte/store'
+import type { TopTFIDF, WordOccurrences } from './types'
+
+async function getData() {
+    const worker = new Worker('./fetch-worker.js')
+    const remoteFetch = Comlink.wrap(worker)
+    return remoteFetch
+}
 
 export const wordOccurrences = writable<WordOccurrences>(null);
 (async () => {
-    let wc = {};
-    (await d3.csv('./word_occurrences.csv'))
-        .forEach(({ id, top_words }) => {
-            wc[id] = (0, eval)('(' + top_words + ')')
-        })
+    // (await d3.csv('./word_occurrences.csv'))
+    //     .forEach(({ id, top_words }) => {
+    //         wc2[id] = (0, eval)('(' + top_words + ')')
+    //     })
+
+    // @ts-ignore
+    const wc = await (await getData()).wordOccurrences()
 
     wordOccurrences.set(wc)
 })()
 
 export const topTFIDF = writable<TopTFIDF>(null);
 (async () => {
-    let d = {};
-    (await d3.csv('./top_tfidf.csv'))
-        .forEach(({ id, top_words }) => {
-            d[id] = (0, eval)('(' + top_words + ')')
-        })
+    // (await d3.csv('./top_tfidf.csv'))
+    //     .forEach(({ id, top_words }) => {
+    //         (0, eval)('(' + top_words + ')')
+    //     })
 
+    const d = await (await getData()).topTfidf()
     topTFIDF.set(d)
 })()
