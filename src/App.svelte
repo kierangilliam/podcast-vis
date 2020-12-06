253ce430a27b2b77b9c022b45d09dbaff65c7b63
Kieran Gill <kieran.gill1997@gmail.com>
2020-12-05 22:22:49 -0800

    dynamic favicon/title for podcast

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,56 @@
+<svelte:head>
+	<title>{sites[$ID].metadata.title}</title>
+	<link rel='icon' type='image/png' href='/favicon/{$ID}.ico'/>
+</svelte:head>
+
 <script lang='ts'>
 	import { Theme, Topics, Section, Similarity, ScreenTime, Intro, Methodology } from '@components'
 	import { episodes } from '@lib/data'
 	import { ID } from '@lib/stores'
 	import { Flex, Spacer } from '@ollopa/cedar'
+	import { onMount } from 'svelte'
 
-	if (
-		window.location.host === 'localhost:5000'
-		|| window.location.host === 'joerogan.faith'
-	) {
-		$ID = 'jre'
+	if (window.location.host === 'localhost:5000') $ID = 'lex'
+	if (window.location.host === 'joerogan.faith') $ID = 'jre'
+	if (window.location.host === 'lexfridman.faith') $ID = 'lex'
+
+	onMount(() => {
+		window.scrollTo(0, 0)
+	})
+
+	const defaultSubtitle = '' // TODO
+
+	const sites = {
+		jre: {
+			metadata: {
+				title: 'Average Joe',
+				subtitle: '2462 videos, 8,626,908 words, and 699gb of data analyzed.',
+			}
+		},
+		ymh: {
+			metadata: {
+				title: `Your Mother's House`,
+			}
+		},
+		flagrant2: {
+			metadata: {
+				title: 'Flagrant 2',
+			}
+		},
+		lex: {
+			metadata: {
+				title: 'Lex Fridman',
+			}
+		},
 	}
-
-	console.log(window.location.pathname)
 </script>
 
 <Theme>
 	<Flex column>
 		<Spacer s={8} />
-		<h1 class='title'>Average Joe</h1>
+		<h1 class='title'>{sites[$ID].metadata?.title}</h1>
 		<div class='subtitle'>
-			<i><h5>2462 videos, 8,626,908 words, and 699gb of data analyzed.</h5></i>
+			<i><h5>{sites[$ID].metadata?.subtitle || defaultSubtitle}</h5></i>
 		</div>
 		
 		{#if !$episodes}
